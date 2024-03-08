d93913e8c1f7f6fcd77d68ba27a629b7d47bbd90
Nicolas Ollier <nicolas@lepetitmartin.com>
2024-03-08 09:28:19 +0100

    add user info on navbar

--- app/src/layout/Nav.jsx
+++ app/src/layout/Nav.jsx
@@ -1,23 +1,42 @@
 import { Link } from "react-router-dom";
 import logo from "../img/argentBankLogo.png";
+import { useSelector } from 'react-redux';
 
 const Nav = () => {
+  const profileData = useSelector(state => state.profile.profileData);
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
   return (
-    <nav class="main-nav">
-      <a class="main-nav-logo" href="/">
+    <nav className="main-nav">
+      <a className="main-nav-logo" href="/">
         <img
-          class="main-nav-logo-image"
+          className="main-nav-logo-image"
           src={logo}
           alt="Argent Bank Logo"
         />
-        <h1 class="sr-only">Argent Bank</h1>
+        <h1 className="sr-only">Argent Bank</h1>
       </a>
-      <div>
-        <Link class="main-nav-item" to="/signin">
-          <i class="fa fa-user-circle"></i>
-          Sign In
-        </Link>
-      </div>
+
+      {isAuthenticated ? (
+        <div id="main-nav-item-wrapper">
+          <Link className="main-nav-item" to="/profile">
+            <i className="fa fa-user"></i>
+            {profileData ? profileData.firstName : 'Profile'}
+          </Link>
+
+          <Link className="main-nav-item" to="/signout">
+            <i className="fa fa-sign-out"></i>
+            Sign Out
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link className="main-nav-item" to="/signin">
+            <i className="fa fa-user"></i>
+            Sign In
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
